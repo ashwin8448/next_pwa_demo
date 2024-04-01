f3abdf8ea3f7fdde5ec7f6d3d2922f9228acc37e
Ashwin Nair <ashwin.nair@qburst.com>
2024-04-01 15:27:44 +0530

    chore: Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      Next PWA Demo Application
+      Next+PWA Demo Application
     </main>
   );
 }
